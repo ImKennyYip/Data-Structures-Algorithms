376750acafc9b6ac70ed162d24f65c81aed14535
ImKennyYip <78777681+ImKennyYip@users.noreply.github.com>
2021-03-21 01:42:16 -0400

    Update 5. Memory Maps Shallow Copy vs Deep Copy.py

--- Memory Maps/5. Memory Maps Shallow Copy vs Deep Copy.py
+++ Memory Maps/5. Memory Maps Shallow Copy vs Deep Copy.py
@@ -7,8 +7,6 @@ lst2[2] = 30
 print("lst1 =", lst1)
 print("lst2 =", lst2)
 
-print()
-
 #2
 lst1 = [1, 2, [3, 4]]
 lst2 = lst1.copy()
@@ -16,8 +14,6 @@ lst1[0] = 10
 lst2[2][0] = 30
 print("lst1 =", lst1)
 print("lst2 =", lst2)
-
-print()
 
 #3
 import copy
